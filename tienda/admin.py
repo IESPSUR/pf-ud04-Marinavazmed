566fd8c7c517447ba565296fc94229102b2760cd
Marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2022-10-31 10:20:30 +0100

    arreglar bbdd

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.db import models
 # Register your models here.
+from .models import *
+
+admin.site.register(Producto)
+admin.site.register(Marca)
+admin.site.register(Compra)
--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=30, primary_key=True)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=30, null=True)
+    modelo = models.CharField(max_length=30, primary_key=True)
+    unidades = models.IntegerField(null=True)
+    precio = models.FloatField(null=True)
+    detalles = models.CharField(max_length=30, null=True)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE, null=True)
+
+
+class Compra(models.Model):
+    fecha = models.DateField()
+    unidades = models.IntegerField()
+    importe = models.FloatField()
+
