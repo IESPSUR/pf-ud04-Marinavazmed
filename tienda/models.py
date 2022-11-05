d90fbea605b31cb3b8d58d83b901cfcaa40df04e
Marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2022-11-05 08:59:56 +0100

    modelo bbdd

--- tienda/models.py
+++ tienda/models.py
@@ -3,19 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Marca(models.Model):
-    nombre = models.CharField(max_length=30, primary_key=True)
+    nombre = models.CharField(max_length=30, unique=True, default='Sin Marca asignada')
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length=30, null=True)
-    modelo = models.CharField(max_length=30, primary_key=True)
+    nombre = models.CharField(max_length=30, null=True, unique=True)
+    modelo = models.CharField(max_length=30, primary_key=True, unique=True)
     unidades = models.IntegerField(null=True)
     precio = models.FloatField(null=True)
     detalles = models.CharField(max_length=30, null=True)
-    marca = models.ForeignKey(Marca, on_delete=models.CASCADE, null=True)
+    marca = models.ForeignKey(Marca, on_delete=models.RESTRICT, verbose_name="Marca", null=True, default=0)
 
 
 class Compra(models.Model):
     fecha = models.DateField()
     unidades = models.IntegerField()
     importe = models.FloatField()
+    nombre = models.ForeignKey('Producto', to_field='nombre', related_name='Nombre', on_delete=models.RESTRICT)
 
