e6b9ac1dc4dc46ea10ff646dca1cbde981a58ff8
Marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2022-11-05 10:18:14 +0100

    listado

--- tienda/models.py
+++ tienda/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.forms import ModelForm
+
 
 # Create your models here.
 
 class Marca(models.Model):
     nombre = models.CharField(max_length=30, unique=True, default='Sin Marca asignada')
+    def __str__(self):
+        return self.nombre
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=30, null=True, unique=True)
@@ -13,10 +17,13 @@ class Producto(models.Model):
     detalles = models.CharField(max_length=30, null=True)
     marca = models.ForeignKey(Marca, on_delete=models.RESTRICT, verbose_name="Marca", null=True, default=0)
 
+    def __str__(self):
+        return self.nombre
+
+
 
 class Compra(models.Model):
     fecha = models.DateField()
     unidades = models.IntegerField()
     importe = models.FloatField()
     nombre = models.ForeignKey('Producto', to_field='nombre', related_name='Nombre', on_delete=models.RESTRICT)
-
--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def welcome(request):
     return render(request,'tienda/index.html', {})
+
+def listado(request):
+    all_objects = Producto.objects.all()
+    context = {'all_objects': all_objects}
+    return render(request, 'tienda/listado.html', context)
