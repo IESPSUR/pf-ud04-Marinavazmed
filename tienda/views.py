3bcc83457d0cf0409d7d22276299d4c0ccb01abb
Marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2022-11-05 18:22:47 +0100

    listado query terminado

--- tienda/views.py
+++ tienda/views.py
@@ -6,7 +6,6 @@ def welcome(request):
     return render(request,'tienda/index.html', {})
 
 def listado(request):
-#    all_objects = list(Producto.objects.all().values_list('nombre', flat=True))
-    all_objects = Producto.objects.all().values('nombre')
-    context = {'all_objects': all_objects}
-    return render(request, 'tienda/listado.html', context)
+    all_objects = Producto.objects.all().values()
+    listaquery = list(all_objects)
+    return render(request, 'tienda/listado.html', {'listaquery' : listaquery})
