bb728156365e253201bfc2527878e79ee0722b57
Marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2022-11-05 10:39:00 +0100

    listado queryset

--- tienda/views.py
+++ tienda/views.py
@@ -6,6 +6,6 @@ def welcome(request):
     return render(request,'tienda/index.html', {})
 
 def listado(request):
-    all_objects = Producto.objects.all()
+    all_objects = Producto.objects.all().values('nombre')
     context = {'all_objects': all_objects}
     return render(request, 'tienda/listado.html', context)
